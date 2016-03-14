3da851e668a9b830f5f82c21728dd9f78cca777a
Smit <smit@cloudlabs.io>
2016-03-13 20:12:33 -0700

    First commit

---
+++ src/main/java/com/test/Demo.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by Smit on 3/13/16.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello world! Args: " + args[0] + " " + args[1]);
+    }
+}
